eee80bb5e915d00325d45a395a573085dca5d60a
Drew Hoek <drewh815@gmail.com>
2023-01-27 15:59:32 -0600

    Basic CSS added

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,12 +8,14 @@ function App() {
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
         <p>Gallery goes here</p>
+        <div id="gallery-container">
         <img src="images/goat_small.jpg"/>
         <img src="images/Bemidji.jpeg"/>
         <img src="images/Colorado.jpeg"/>
         <img src="images/Cooking-Demonstration.jpeg"/>
         <img src="images/Family-Christmas.jpeg"/>
         <img src="images/Moby-Christmas.jpeg"/>
+        </div>
       </div>
     );
 }
