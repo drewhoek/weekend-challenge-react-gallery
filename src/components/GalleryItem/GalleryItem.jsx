a332ad8f0ec9f152ef8a39d9a7ce9986972016e0
Drew Hoek <drewh815@gmail.com>
2023-01-28 18:18:44 -0600

    Base goals done

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,6 +1,9 @@
 import axios from "axios";
+import { useState } from "react";
 
 function GalleryItem({ pic, fetchPics }) {
+
+  const [displayMode, setDisplayMode] = useState(false)
 
     function likePicture() {
         console.log('liking picture at id:', pic.id);
@@ -14,11 +17,29 @@ function GalleryItem({ pic, fetchPics }) {
           console.log("error in liking picture", err);
         });
     }
+
+function toggleMode() {
+  console.log('switching mode of list item');
+  setDisplayMode(!displayMode);
+}
+
+const whichDisplay = () => {
+  const picDisplay = <img src= {pic.path}/>
+  const descriptionDisplay = <p>{pic.description}</p>
+  if (displayMode) {
+    return descriptionDisplay
+  } else {
+    return picDisplay
+  }
+}
+
+console.log(whichDisplay());
     return (
         <>
              <div>
-                <img src= {pic.path}/>
-                <p>{pic.description}</p>
+                <div class ="displayed-div" onClick={toggleMode}>
+                  {whichDisplay(displayMode)}
+                </div>
                 <button className="like-button" onClick={likePicture}>Like this Picture!</button>
                 <p>{pic.likes} people have liked this picture</p>
             </div>
